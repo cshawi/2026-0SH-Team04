003d4cf1aa0d0f9a690d64cf827027c0c689dcc8
Merveille-pharel1 <pharelkamga777@gmail.com>
2026-03-05 13:16:38 -0500

    Mise en place frontend et backend

---
+++ backend-server/src/controllers/generate.controller.js
@@ -0,0 +1,3 @@
+exports.generate = (req, res) => {
+  res.json({ message: "generate" });
+};
---
+++ backend-server/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+router.post("/register", (req, res) => {
+  res.json({ message: "register" });
+});
+
+router.post("/login", (req, res) => {
+  res.json({ message: "login" });
+});
+
+module.exports = router;
